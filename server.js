4d107ac9c41e567ef2de82dc7f94153f39d33ba7
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-09-16 09:33:31 -0500

    logger file added

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 'use strict';
 
 const express = require('express');
+const logger = require('./middleware/logger.js');
 
 // creates an express singleton (a single instance of express).
 const app = express();
 
+app.use(express.json());
 
+app.use(logger);
 
+app.use('/', (req, res, next) => {
+    res.status(200).send('Hello World!');
+});
+
+app.listen(3001, () => console.log('server is running'));
