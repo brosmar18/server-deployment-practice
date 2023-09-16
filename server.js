43865f5257c365dfb66ffe82e6d3a024d080d089
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-09-16 12:28:13 -0500

    500 errorHandler added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const logger = require('./middleware/logger.js');
 const notFond = require('./handlers/404.js');
+const errorHandler = require('./handlers/500.js');
 
 // creates an express singleton (a single instance of express).
 const app = express();
@@ -15,6 +16,11 @@ app.get('/', logger, (req, res, next) => {
     res.status(200).send(req.log);
 });
 
+app.get('/bad', (req, res, next) => {
+    next('We have an error');
+});
+
 app.use('*', notFond);
+app.use(errorHandler);
 
 app.listen(3001, () => console.log('server is running'));
